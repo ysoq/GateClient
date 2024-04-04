bc16a3f5e3731dacdde55406027f06e4a137fe65
赛 姚 <253711403@qq.com>
2024-04-04 21:19:44 +0800

    1

---
+++ CodeCore/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CodeCore
+{
+    public class Util
+    {
+        public static Color ToColor(string rgb)
+        {
+            return (Color)ColorConverter.ConvertFromString(rgb);
+        }
+
+        public static SolidColorBrush ToBrush(string rgb)
+        {
+            try
+            {
+                return new SolidColorBrush(ToColor(rgb));
+            }
+            catch (Exception)
+            {
+                return Brushes.Red;
+            }
+        }
+    }
+}
--- GateClient/PageControl.xaml.cs
+++ GateClient/PageControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Windows.Media;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace GateClient
@@ -12,8 +13,6 @@ namespace GateClient
         {
             InitializeComponent();
         }
-
-
 
 
         public string StatusName
@@ -38,8 +37,6 @@ namespace GateClient
             DependencyProperty.Register("LeftTopText", typeof(string), typeof(PageControl), new PropertyMetadata(""));
 
 
-
-
         public string RightBottomText
         {
             get { return (string)GetValue(RightBottomTextProperty); }
@@ -49,6 +46,56 @@ namespace GateClient
         // Using a DependencyProperty as the backing store for RightBottomText.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty RightBottomTextProperty =
             DependencyProperty.Register("RightBottomText", typeof(string), typeof(PageControl), new PropertyMetadata(""));
+
+
+        public Color StartBg
+        {
+            get { return (Color)GetValue(StartBgProperty); }
+            set { SetValue(StartBgProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StartBg.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartBgProperty =
+            DependencyProperty.Register("StartBg", typeof(Color), typeof(PageControl), new PropertyMetadata(Color.FromArgb(1, 204, 232, 252)));
+
+
+        public Color EndBg
+        {
+            get { return (Color)GetValue(EndBgProperty); }
+            set { SetValue(EndBgProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for EndBg.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EndBgProperty =
+            DependencyProperty.Register("EndBg", typeof(Color), typeof(PageControl), new PropertyMetadata(Color.FromArgb(1, 59, 185, 243)));
+
+
+
+
+        public Brush ThemeBg
+        {
+            get { return (Brush)GetValue(ThemeBgProperty); }
+            set { SetValue(ThemeBgProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ThemeBg.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThemeBgProperty =
+            DependencyProperty.Register("ThemeBg", typeof(Brush), typeof(PageControl), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(1, 2, 167, 240))));
+
+
+
+
+        public Geometry ThemeIcon
+        {
+            get { return (Geometry)GetValue(ThemeIconProperty); }
+            set { SetValue(ThemeIconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ThemeIcon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThemeIconProperty =
+            DependencyProperty.Register("ThemeIcon", typeof(Geometry), typeof(PageControl));
+
+
 
     }
 }
